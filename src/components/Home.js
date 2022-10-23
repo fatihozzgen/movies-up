0b43a50f3c9aacccd849f17d149265d4a2300b8f
fatihozzgen <fatihozzgen@gmail.com>
2022-10-24 00:13:26 +0300

    1.2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,20 @@
 import React from "react";
 import People from "../logo/people.png";
 import Yellow from "../logo/yellow.png";
+import Movieup from "../logo/wmovieup.png";
+import Circle from "../logo/circle.png";
+import Lorem from "../logo/lorem.png";
 
 function Home() {
   return (
     <div className="home">
       <div className="welcome">
-        <img src={People} />
-        <img src={Yellow} />
+        <img className="wbackground" src={People} />
+        <img className="wbackground" src={Yellow} />
+
+        <img className="wmovieup  " src={Movieup} />
+        <img className="wcircle" src={Circle} />
+        <img className="wlorem" src={Lorem} />
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,28 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Movie from "../logo/movie.png";
 import Up from "../logo/up.png";
+import { RiSearchLine } from "react-icons/ri";
 
 function Navbar() {
   return (
     <>
       <div className="navbar">
-        <div className="movie-logo">
-          <img src={Movie} />
-          {/* <img className="up-logo" src={Up} /> */}
-        </div>
+        <div className="nav-container">
+          <div className="movie-logo">
+            <img src={Movie} />
+            {/* <img className="up-logo" src={Up} /> */}
+          </div>
 
-        <div className="navigation">
-          <Link to="/">
-            <div> Home </div>{" "}
-          </Link>
+          <div className="navigation">
+            <Link to="/">
+              <div> Home </div>{" "}
+            </Link>
 
-          <Link to="/favorite">
-            <div className="button-fav">Favorite</div>
-          </Link>
-        </div>
+            <Link to="/favorite">
+              <div className="button-fav">Favorite</div>
+            </Link>
+          </div>
 
-        <div className="nav-input">
-          <input />
+          <div className="nav-input">
+            <input placeholder="Enter movie name here" />
+            <RiSearchLine size={25} color={"#A4A4A4"} />
+          </div>
         </div>
       </div>
     </>
