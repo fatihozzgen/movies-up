99f7d900ee64ed64a0c3e17a7214dcb9eeafb6ca
fatihozzgen <fatihozzgen@gmail.com>
2022-11-08 23:19:10 +0300

    context

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Favorites from "./components/Favorites";
+import { mainContext } from "./context";
+
 import "./styles/navbar.css";
 import "./styles/home.css";
 import "./styles/filter.css";
@@ -10,10 +12,18 @@ import "./styles/favorites.css";
 import "./styles/popular.css";
 
 import "swiper/css/bundle";
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("istanbul");
+
+  const datax = {
+    search,
+    setSearch,
+  };
+
   return (
-    <div>
+    <mainContext.Provider value={datax}>
       <BrowserRouter>
         <Navbar />
         <Routes>
@@ -21,7 +31,7 @@ function App() {
           <Route path="/favorite" element={<Favorites />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </mainContext.Provider>
   );
 }
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Up from "../logo/up.png";
 import { RiSearchLine } from "react-icons/ri";
+import { mainContext, useContext } from "../context";
 
 function Search() {
+  const { search, setSearch } = useContext(mainContext);
+  console.log(search);
+
   return (
     <div className="nav-input">
-      <input placeholder="Enter movie name here" />
+      <input
+        placeholder="Enter movie name here"
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
       <div className="search-icon">
         <RiSearchLine style={{ cursor: "pointer" }} />
